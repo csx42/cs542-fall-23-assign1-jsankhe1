19b16c529d6abcd4092963edea1c304c168eb951
jsankhe1 <jsankhe1@binghamton.edu>
2023-09-19 12:28:28 -0400

    Removed the Generator classes for student and course, created a new class that could do the functionality of both

--- studentCoursesMgmt/src/studentCoursesMgmt/util/CourseInfoGeneratorInterface.java
+++
@@ -1,7 +0,0 @@
-//package studentCoursesMgmt.util;
-//
-//import java.util.List;
-//
-//public interface CourseInfoGeneratorInterface {
-//    public List<CourseImpl> getCourseInfo(List<String> courseInfoList);
-//}
--- studentCoursesMgmt/src/studentCoursesMgmt/util/StudentPrefsGeneratorInterface.java
+++
@@ -1,8 +0,0 @@
-//package studentCoursesMgmt.util;
-//
-//import java.util.List;
-//
-//public interface StudentPrefsGeneratorInterface {
-//
-//    public List<StudentImpl> createStudentData(List<String> studentPrefList);
-//}
